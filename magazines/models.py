7168c3cb5108afa96d8647963139e1f22d4f5fde
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-03-08 23:00:19 +0330

    v 1.5 (magazines models fixed)

--- magazines/models.py
+++ magazines/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Authors(models.Model):
+    name = models.CharField(max_length= 20)
+    national_ID = models.CharField(max_length= 10, unique=True)
+    phone = models.CharField(max_length= 11, unique= True)
+    email = models.EmailField(null = True, blank= True)
+
+
+class Users(models.Model):
+    name = models.CharField(max_length= 20)
+    national_ID = models.CharField(max_length= 10, unique=True)
+    phone = models.CharField(max_length= 11, unique= True)
+    email = models.EmailField(null = True, blank= True)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=20)
+    national_id = models.CharField(max_length= 10, unique= True)
+    establishment = models.DateField()
+    email = models.EmailField()
+
+
+class Magazines(models.Model):
+    name = models.CharField(max_length= 20)
+    number = models.CharField(max_length=20, unique= True)
+    authors = models.ManyToManyField(Authors, related_name= 'narrator')
+    users = models.ManyToManyField(Users, related_name='users')
+    price = models.IntegerField()
+    
+    
